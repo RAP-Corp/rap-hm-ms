7ac0f59511ce580f72eb86b4c831a57e191e9a3a
Brijesh Singh <brijeshsingh@xav-us-mac1330.local>
2018-07-02 14:37:17 -0400

    first commit

---
+++ src/main/java/com/rap/hm/raphmms/RapHmMsApplication.java
@@ -0,0 +1,12 @@
+package com.rap.hm.raphmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapHmMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapHmMsApplication.class, args);
+	}
+}
